bf3d1337c4ef1c43b971bb5e3f29f0f56df773bc
Qvverty72 <car.sotoa@duocuc.cl>
2024-06-20 22:10:07 -0400

    added more models

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Bicicleta
+from .models import User, Bicicleta, Servicio, Accesorio, Producto, Boleta, DetalleBoleta, Delivery, Pago
 admin.site.register(User)
 admin.site.register(Bicicleta)
+admin.site.register(Servicio)
+admin.site.register(Accesorio)
+admin.site.register(Producto)
+admin.site.register(Boleta)
+admin.site.register(DetalleBoleta)
+admin.site.register(Delivery)
+admin.site.register(Pago)
+
