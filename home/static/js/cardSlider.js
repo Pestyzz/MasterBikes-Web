747ccabcd7f6214b37cd060c3cc42a5a268f2de1
Bastián Ñiripil <ba.niripil@duocuc.cl>
2024-06-23 20:46:34 -0400

    implement: services slider + new styles

--- home/static/js/cardSlider.js
+++ home/static/js/cardSlider.js
@@ -1,19 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let multipleCardCarousel = document.querySelector("#carouselBikes");
+    let multipleCardCarousel1 = document.querySelector("#carouselBikes");
     let multipleCardCarousel2 = document.querySelector("#carouselAccesories");
+    let multipleCardCarousel3 = document.querySelector("#carouselServices");
 
     if (window.matchMedia("(min-width: 768px)").matches) {
-        let carousel1 = new bootstrap.Carousel(multipleCardCarousel, {
-            interval: true, // Disable automatic sliding
-            wrap: true, // Prevent wrapping at the end
+        let carousel1 = new bootstrap.Carousel(multipleCardCarousel1, {
+            interval: false,
+            wrap: false,
         });
         let carousel2 = new bootstrap.Carousel(multipleCardCarousel2, {
-            interval: true, // Disable automatic sliding
-            wrap: true, // Prevent wrapping at the end
+            interval: false,
+            wrap: false,
+        });
+        let carousel3 = new bootstrap.Carousel(multipleCardCarousel3, {
+            interval: false,
+            wrap: false,
         });
 
-        let carouselInner1 = multipleCardCarousel.querySelector(".carousel-inner");
-        let carouselItems1 = multipleCardCarousel.querySelectorAll(".carousel-item");
+        let carouselInner1 = multipleCardCarousel1.querySelector(".carousel-inner");
+        let carouselItems1 = multipleCardCarousel1.querySelectorAll(".carousel-item");
         let carouselWidth1 = carouselInner1.scrollWidth;
         let cardWidth1 = carouselItems1[0].offsetWidth;
         let scrollPosition1 = 0;
@@ -24,8 +29,14 @@ document.addEventListener('DOMContentLoaded', function () {
         let cardWidth2 = carouselItems2[0].offsetWidth;
         let scrollPosition2 = 0;
 
+        let carouselInner3 = multipleCardCarousel3.querySelector(".carousel-inner");
+        let carouselItems3 = multipleCardCarousel3.querySelectorAll(".carousel-item");
+        let carouselWidth3 = carouselInner3.scrollWidth;
+        let cardWidth3 = carouselItems3[0].offsetWidth;
+        let scrollPosition3 = 0;
+
         document.querySelector("#carouselBikes .carousel-control-next").addEventListener("click", function () {
-            if (scrollPosition1 < carouselWidth1 - cardWidth1 * 4) {
+            if (scrollPosition1 < carouselWidth1 - cardWidth1 * 3) {
                 scrollPosition1 += cardWidth1;
                 carouselInner1.scroll({ left: scrollPosition1, behavior: 'smooth' });
             }
@@ -39,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         document.querySelector("#carouselAccesories .carousel-control-next").addEventListener("click", function () {
-            if (scrollPosition2 < carouselWidth2 - cardWidth2 * 4) {
+            if (scrollPosition2 < carouselWidth2 - cardWidth2 * 3) {
                 scrollPosition2 += cardWidth2;
                 carouselInner2.scroll({ left: scrollPosition2, behavior: 'smooth' });
             }
@@ -51,8 +62,23 @@ document.addEventListener('DOMContentLoaded', function () {
                 carouselInner2.scroll({ left: scrollPosition2, behavior: 'smooth' });
             }
         });
+
+        document.querySelector("#carouselServices .carousel-control-next").addEventListener("click", function () {
+            if (scrollPosition3 < carouselWidth3 - cardWidth3 * 3) {
+                scrollPosition3 += cardWidth3;
+                carouselInner3.scroll({ left: scrollPosition3, behavior: 'smooth' });
+            }
+        });
+
+        document.querySelector("#carouselServices .carousel-control-prev").addEventListener("click", function () {
+            if (scrollPosition3 > 0) {
+                scrollPosition3 -= cardWidth3;
+                carouselInner3.scroll({ left: scrollPosition3, behavior: 'smooth' });
+            }
+        });
     } else {
-        multipleCardCarousel.classList.add("slide");
-        multipleCardCarousel2.classList.add("slide");
+        multipleCardCarousel1.classList.add('slide');
+        multipleCardCarousel2.classList.add('slide');
+        multipleCardCarousel3.classList.add('slide');
     }
 });
